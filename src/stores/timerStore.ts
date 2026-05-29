6769ed618a29213a06d7384e9173347c6d0f4dda
mazwiibaxolele <mazwiibaxolele@gmail.com>
2026-05-29 13:04:41 +0200

    Fix TypeScript errors in timerStore

--- src/stores/timerStore.ts
+++ src/stores/timerStore.ts
@@ -24,6 +24,7 @@ interface TimerStore {
   sessionStartedAt: string | null;
   breaksTaken: number;
   showCompletionModal: boolean;
+  targetTime: number | null;
 
   // Actions
   startTimer: (moduleId: string, method: StudyMethod) => void;
